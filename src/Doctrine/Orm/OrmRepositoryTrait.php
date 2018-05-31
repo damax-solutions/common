0d330294d72f4163c1d62a87e6e497ebd80f21b0
Dmitri Lakachauskis <lakiboy83@gmail.com>
2018-05-31 11:09:51 +0300

    OrmRepositoryTrait for ORM repos.

---
+++ src/Doctrine/Orm/OrmRepositoryTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Damax\Common\Doctrine\Orm;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+
+trait OrmRepositoryTrait
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var string
+     */
+    private $className;
+
+    private function createQueryBuilder(string $alias): QueryBuilder
+    {
+        return $this->em
+            ->createQueryBuilder()
+            ->select($alias)
+            ->from($this->className, $alias)
+        ;
+    }
+}
